ef6628b4fdb201884420722f930f007055ca5abb
Eirik Strøm <eirik@visito.no>
2022-04-14 11:51:38 +0200

    Get RabbitMQ working

--- hworld-client/main.go
+++ hworld-client/main.go
@@ -10,6 +10,7 @@ import (
 	pb "hworld-client/grpc"
 
 	"github.com/gin-gonic/gin"
+	"github.com/streadway/amqp"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
@@ -75,9 +76,55 @@ func listen(data *data) {
 	router.Run(":80")
 }
 
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
 func main() {
 	flag.Parse()
-	data := data{}
-	talkToServer(&data)
-	listen(&data)
+	//	data := data{}
+	//	talkToServer(&data)
+	//	listen(&data)
+
+	conn, err := amqp.Dial("amqp://default_user_8Umc7dr4LB1yLDzbY0T:ZC8Z-05HvbqlYBlhh44fep9j36ESp_Xf@definition.default.svc.cluster.local:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
 }
--- hworld-server/main.go
+++ hworld-server/main.go
@@ -10,6 +10,7 @@ import (
 
 	pb "hworld-client/grpc"
 
+	"github.com/streadway/amqp"
 	"google.golang.org/grpc"
 )
 
@@ -46,8 +47,7 @@ func (s server) FetchResponse(in *pb.Request, srv pb.StreamService_FetchResponse
 	return nil
 }
 
-func main() {
-	flag.Parse()
+func setUpgRPCServer() {
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
@@ -59,5 +59,50 @@ func main() {
 	// and start...
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	//setUpgRPCServer()
+	conn, err := amqp.Dial("amqp://default_user_8Umc7dr4LB1yLDzbY0T:ZC8Z-05HvbqlYBlhh44fep9j36ESp_Xf@definition.default.svc.cluster.local:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	for {
+		body := "Hello World!"
+		err = ch.Publish(
+			"",     // exchange
+			q.Name, // routing key
+			false,  // mandatory
+			false,  // immediate
+			amqp.Publishing{
+				ContentType: "text/plain",
+				Body:        []byte(body),
+			})
+		failOnError(err, "Failed to publish a message")
+
+		time.Sleep(20 * time.Second)
 	}
 }
