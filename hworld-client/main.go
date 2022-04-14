9ae73a16afa457081a2861acf9819c319dc4b1d1
Eirik Strøm <eirik@visito.no>
2022-04-14 23:23:17 +0200

    Remove unused code

--- hworld-client/main.go
+++ hworld-client/main.go
@@ -1,81 +1,12 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"fmt"
-	"io"
 	"log"
-	"net/http"
 
-	pb "hworld-client/grpc"
-
-	"github.com/gin-gonic/gin"
 	"github.com/streadway/amqp"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 )
-
-const (
-	defaultName = "world"
-)
-
-var (
-	addr = flag.String("addr", "hworld-server-service.default.svc.cluster.local:8080", "the address to connect to")
-	name = flag.String("name", defaultName, "Name to greet")
-)
-
-func getNumbers(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, struct {
-		Num int `json:"num"`
-	}{Num: 1})
-}
-
-type data struct {
-	Num string
-}
-
-func talkToServer(data *data) {
-	// Set up a connection to the server.
-	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		log.Fatalf("did not connect: %v", err)
-	}
-
-	client := pb.NewStreamServiceClient(conn)
-	in := &pb.Request{Id: 1}
-	stream, err := client.FetchResponse(context.Background(), in)
-	if err != nil {
-		log.Fatalf("open stream error %v", err)
-	}
-
-	done := make(chan bool)
-
-	go func() {
-		for {
-			resp, err := stream.Recv()
-			if err == io.EOF {
-				done <- true //means stream is finished
-				return
-			}
-			if err != nil {
-				log.Fatalf("cannot receive %v", err)
-			}
-			data.Num = resp.Result
-		}
-	}()
-}
-
-func listen(data *data) {
-	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": data.Num,
-		})
-	})
-
-	router.Run(":80")
-}
 
 func failOnError(err error, msg string) {
 	if err != nil {
@@ -85,9 +16,6 @@ func failOnError(err error, msg string) {
 
 func main() {
 	flag.Parse()
-	//	data := data{}
-	//	talkToServer(&data)
-	//	listen(&data)
 
 	rabbitmq_username := GetConfig("rabbitmq_username")
 	rabbitmq_password := GetConfig("rabbitmq_password")
--- hworld-server/main.go
+++ hworld-server/main.go
@@ -4,63 +4,10 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"net"
-	"sync"
 	"time"
 
-	pb "hworld-client/grpc"
-
 	"github.com/streadway/amqp"
-	"google.golang.org/grpc"
 )
-
-var (
-	port = flag.Int("port", 80, "The server port")
-)
-
-type server struct {
-	pb.UnimplementedStreamServiceServer
-}
-
-func (s server) FetchResponse(in *pb.Request, srv pb.StreamService_FetchResponseServer) error {
-
-	log.Printf("fetch response for id : %d", in.Id)
-
-	//use wait group to allow process to be concurrent
-	var wg sync.WaitGroup
-	for i := 0; i < 5; i++ {
-		wg.Add(1)
-		go func(count int64) {
-			defer wg.Done()
-
-			//time sleep to simulate server process time
-			time.Sleep(time.Duration(count) * time.Second)
-			resp := pb.Response{Result: fmt.Sprintf("Request #%d For Id:%d", count, in.Id)}
-			if err := srv.Send(&resp); err != nil {
-				log.Printf("send error %v", err)
-			}
-			log.Printf("finishing request number : %d", count)
-		}(int64(i))
-	}
-
-	wg.Wait()
-	return nil
-}
-
-func setUpgRPCServer() {
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-	s := grpc.NewServer()
-	pb.RegisterStreamServiceServer(s, server{})
-
-	log.Println("start server")
-	// and start...
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
-}
 
 func failOnError(err error, msg string) {
 	if err != nil {
